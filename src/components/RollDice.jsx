36df9131f3fd4617aa65b5d22d0c990257039088
BiveKKarki <karki777bivek@gmail.com>
2023-08-24 01:59:14 +0545

    onclick math function

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
 const RollDice = () => {
+
+    const [currentDice, setCurrentDice] = useState();
+    const generateRandomNumber = (min, max)=> {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
   return (
     <>
-        
+        <DiceContainer>
+            <div className='dice' onClick={()=> generateRandomNumber(1,6)}>
+                <img src='./images/dice/dice_1.png' alt='dice_1' />
+            </div>
+            <p>Click on Dice to roll</p>
+        </DiceContainer>
     </>
   )
 }
 
 export default RollDice
+
+const DiceContainer = styled.div`
+    margin-top: 48px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .dice{
+        cursor: pointer;
+
+    }
+    p{
+        font-size: 24px;
+    }
+
+`;
